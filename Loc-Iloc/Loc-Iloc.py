38c08a2739eae4cd624534b85b837ce37ad10d2f
Lombardi <lulombardi@deloitte.it>
2024-01-17 15:06:29 +0100

    Loc-Iloc

--- Loc-Iloc/Loc-Iloc.py
+++ Loc-Iloc/Loc-Iloc.py
@@ -7,4 +7,30 @@ df = pd.DataFrame({'Day': ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
                    'Humidity': [24, 26, 36, 50, 26, 35, 24]}).set_index('Day')
 
 
-print(df)
+#Selecting a single value
+print(df.loc['Tue', 'Temperature'])
+print(df.iloc[1, 1])
+
+#Selectig all rows of a specific column
+print(df.loc[:, 'Temperature'])
+print(df.iloc[:, 1])
+
+#Selecting all columns of a specific row
+print(df.loc['Mon', :])
+print(df.iloc[0, :])
+
+#selcting list of values
+print(df.loc[['Mon', 'Tue'], 'Wind'])
+print(df.iloc[[0, 1], 2])
+print(df.loc['Thu', ['Temperature', 'Wind']])
+print(df.iloc[3, [1, 2]])
+
+#Slicing
+print(df.loc['Mon':'Sun', 'Weather'])
+print(df.iloc[0:7, 0])
+print(df.loc['Mon', 'Weather':'Humidity'])
+print(df.iloc[0, 0:4])
+print(df.loc['Mon':'Fri':2, :])
+print(df.iloc[0:5:2, :])
+
+#Conditions and callable
